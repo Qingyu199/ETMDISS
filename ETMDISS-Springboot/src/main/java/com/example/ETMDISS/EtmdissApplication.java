aa4c7d88bb0cb449b4d918a4d19875ddba784978
yuqing <yuqing.nj@gmail.com>
2025-05-27 16:21:32 +0200

    Initial commit

---
+++ ETMDISS-Springboot/src/main/java/com/example/ETMDISS/EtmdissApplication.java
@@ -0,0 +1,13 @@
+package com.example.ETMDISS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtmdissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtmdissApplication.class, args);
+	}
+
+}
---
+++ ETMDISS-Vue/src/test/java/com/example/ETMDISS/EtmdissApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ETMDISS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtmdissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
